6ee6fe1cc2f96011befbfbd2b7f6943a887d879b
marwensaidana <marwen005@hotmail.com>
2016-06-17 11:28:29 +0200

    p

--- js/script.js
+++ js/script.js
@@ -29,6 +29,95 @@ default:
 }
 });
 });
+/*
+*****Colistion coins 
 
 
+var Coin = Item.extend({
+	init: function(x, y, level) {
+		this._super(x, y, false, level);
+		this.setImage(images.objects, 0, 0);
+		this.setupFrames(10, 4, true);
+	},
+	activate: function(from) {
+		if(!this.activated) {
+			this.level.playSound('coin');
+			from.addCoin();
+			this.remove();
+		}
+		this._super(from);
+	},
+	remove: function() {
+		this.view.remove();
+	},
+}, 'coin');
+var CoinBoxCoin = Coin.extend({
+	init: function(x, y, level) {
+		this._super(x, y, level);
+		this.setImage(images.objects, 96, 0);
+		this.clearFrames();
+		this.view.hide();
+		this.count = 0;
+		this.frames = Math.floor(150 / constants.interval);
+		this.step = Math.ceil(30 / this.frames);
+	},
+	remove: function() { },
+	addToGrid: function() { },
+	addToLevel: function() { },
+	activate: function(from) {
+		this._super(from);
+		this.view.show().css({ 'bottom' : '+=8px' });
+	},
+	act: function() {
+		this.view.css({ 'bottom' : '+=' + this.step + 'px' });
+		this.count++;
+		return (this.count === this.frames);
+	},
+});
+var CoinBox = Item.extend({
+	init: function(x, y, level, amount) {
+		this._super(x, y, true, level);
+		this.setImage(images.objects, 346, 328);
+		this.setAmount(amount || 1);
+	},
+	setAmount: function(amount) {
+		this.items = [];
+		this.actors = [];
+		
+		for(var i = 0; i < amount; i++)
+			this.items.push(new CoinBoxCoin(this.x, this.y, this.level));
+	},
+	activate: function(from) {
+		if(!this.isBouncing) {
+			if(this.items.length) {
+				this.bounce();
+				var coin = this.items.pop();
+				coin.activate(from);
+				this.actors.push(coin);
+				
+				if(!this.items.length)
+					this.setImage(images.objects, 514, 194);
+			}
+		}
+			
+		this._super(from);
+	},
+	playFrame: function() {
+		for(var i = this.actors.length; i--; ) {
+			if(this.actors[i].act()) {
+				this.actors[i].view.remove();
+				this.actors.splice(i, 1);
+			}
+		}
+		
+		this._super();
+	},
+}, 'coinbox');
+var MultipleCoinBox = CoinBox.extend({
+	init: function(x, y, level) {
+		this._super(x, y, level, 8);
+	},
+}, 'multiple_coinbox');
+
+*/
 });
